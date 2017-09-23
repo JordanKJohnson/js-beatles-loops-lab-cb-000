ad39ef360e2da619612a342703137a9a8097e631
Jordan Johnson <jkj86@hotmail.com>
2017-09-23 02:47:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
 
-  var sentences[];
+  var sentences = new array;
   for(var i = 0; i < musicians.length; i++){
     var.push(musicians[i]+" plays "+instruments[i]);
   }
